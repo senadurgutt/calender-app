a3bd874ac35f1c10fd606393bf8a6a1788bd0e78
eliftuncc21 <100561767+eliftuncc21@users.noreply.github.com>
2023-05-29 22:52:31 +0300

    Add files via upload

---
+++ Arch.Core/Constants/StaticParams.cs
@@ -0,0 +1,10 @@
+﻿namespace Arch.Core.Constants
+{
+    public struct StaticParams
+    {
+        public const string Dashboard = "/Dashboard";
+        public const string Member = "/Member";
+        public const string SifrelemeParametresi = "testfalan";
+        public const string DefaultConnstr = "Server=SENADURGUT;Database=Homework;Trusted_Connection=True;MultipleActiveResultSets=true";
+    }
+}
