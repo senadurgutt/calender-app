1d5c7f813d4d589cba29992941fd3e268fc01f3e
eliftuncc21 <100561767+eliftuncc21@users.noreply.github.com>
2023-05-30 11:13:03 +0300

    Add files via upload

---
+++ Arch.Web.Framework/Enums/MessageTitleTypes.cs
@@ -0,0 +1,10 @@
+﻿namespace Arch.Web.Framework.Enums
+{
+    public class MessageTitleTypes
+    {
+        public const string Tebrikler = "Congratulations";
+        public const string Uyari = "Warning";
+        public const string Bilgilendirme = "To inform";
+        public const string Yasak = "Forbidden";
+    }
+}
